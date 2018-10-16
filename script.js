572b48d7792273aa2c48488ec6fdf7f0a179c671
Stanislav Luchankin <luchano057@gmail.com>
2018-10-16 20:31:56 +0700

    Add click on the button

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function (ev) {
+
+var button = document.querySelector('button');
+
+button.addEventListener('click', function() { 
+alert(" Button activated")
+		});
+});
